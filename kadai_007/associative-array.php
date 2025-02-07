9d29b232f37cf703a36c07d1ca0fbe03410ca788
normal-yoko <gijigiji399@gmail.com>
2025-02-08 08:24:55 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編</title>
+</head>
+  <p>
+
+
+      <?php
+      //連想配列に値を代入
+      $array =
+       [
+        'name' => 'orion',
+        'price' => '200',
+        'weight' => '160'
+       ];
+
+      //連想配列の値を出力
+      print_r($array);
+
+      ?>
+  </p>
+</body>
+</html>
